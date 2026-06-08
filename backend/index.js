4ce9b0f6be38bfbbecc53aac4082a07469a1736a
ElliotPa1m <elliot.palm@hotmail.com>
2026-06-08 13:31:00 +0200

    fixed so that I can actually start the server without errors

--- backend/index.js
+++ backend/index.js
@@ -1,21 +1,29 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
+import { MongoClient, Db } from "mongodb";
+import dns from "node:dns";
 
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 dotenv.config()
 
 const app = express()
+const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("Ansluten till MongoDB"))
-    .catch((err) => console.log(err))
+console.log(process.env.MONGO_URI)
 
-const PORT = process.env.PORT || 5000
+const client = new MongoClient(process.env.MONGO_URI) 
 
-app.listen(PORT, () => {
+async function startServer() {
+// let connection =  await client.connect()
+  console.log("Ansluten till MongoDB")
+// connection.db("mystore").collection("orders").insertOne({email:""})
+  app.listen(PORT, () => {
     console.log(`Servern körs på port ${PORT}`)
-})
+  })
+}
+
+startServer()
