631dc693aa12e9be34660e8c6da68463d8305a2f
ElliotPa1m <elliot.palm@hotmail.com>
2026-06-30 11:12:27 +0200

    made it so that the admin can edit and delete products from the list of products

--- frontend/src/pages/admin/AdminProducts.jsx
+++ frontend/src/pages/admin/AdminProducts.jsx
@@ -1,3 +1,33 @@
-export default function ProductsPage() {
-  return <h1>AdminProducts</h1>
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+export default function AdminProducts() {
+
+  const [products, setProducts] = useState([])
+  const handleDelete = async (id) => {
+    await fetch(`http://localhost:5000/api/products/${id}`, {
+      method: "DELETE",
+      headers: {Authorization: `Bearer ${localStorage.getItem("token")}`}
+    })
+    setProducts(products.filter(p => p._id !== id))
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/products")
+      .then(res => res.json())
+      .then(data => setProducts(data))
+  }, [])
+
+  return (
+    <div>
+      {products.map(product => (
+        <div key={product._id}>
+          <h2>{product.name}</h2>
+          <p>${product.price}</p>
+          <Link to={`/admin/products/${product._id}`}>Edit</Link>
+          <button onClick={() => handleDelete(product._id)}>Delete</button>
+        </div>
+      ))}
+    </div>
+  )
 }
