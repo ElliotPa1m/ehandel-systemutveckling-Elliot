93ebb8b1487c2e02d06f41408429f5bf84a90245
ElliotPa1m <elliot.palm@hotmail.com>
2026-07-09 10:31:33 +0200

    wrote the create new product code, after some bugs finally got it to work (some 500 errors because the body JSON.stringify read age and price as a string and not a number

--- frontend/src/pages/admin/AdminNewProductPage.jsx
+++ frontend/src/pages/admin/AdminNewProductPage.jsx
@@ -1,3 +1,35 @@
-export default function ProductsPage() {
-  return <h1>AdminNewProductPage</h1>
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+export default function AdminNewProductPage() {
+
+const [name, setName] = useState("")
+const [age, setAge] = useState("")
+const [description, setDescription] = useState("")
+const [price, setPrice] = useState("")
+const [imageUrl, setImageUrl] = useState("")
+const [category, setCategory] = useState("")
+const navigate = useNavigate()
+
+const handleSubmit = async (e) => {
+    e.preventDefault()
+    const response = await fetch("http://localhost:5000/api/products", {
+      method: "POST",
+      headers: {"Content-Type": "application/json", "Authorization": `Bearer ${localStorage.getItem("token")}`},
+      body: JSON.stringify({name, age: Number(age), description, price: Number(price), imageUrl, category})
+    })
+    navigate("/admin/products")
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)}></input>
+      <input type="number" placeholder="Age" value={age} onChange={(e) => setAge(e.target.value)}></input>
+      <input type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)}></input>
+      <input type="number" placeholder="Price" value={price} onChange={(e) => setPrice(e.target.value)}></input>
+      <input type="text" placeholder="Image" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)}></input>
+      <input type="text" placeholder="Category" value={category} onChange={(e) => setCategory(e.target.value)}></input>
+      <button type="submit">Create</button>
+    </form>
+  )
 }
