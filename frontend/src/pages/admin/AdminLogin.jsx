ef80b61a0246a8f4f5e070d6d1cdc59408486d19
ElliotPa1m <elliot.palm@hotmail.com>
2026-08-17 13:43:09 +0200

    added host variables

--- frontend/src/pages/admin/AdminLogin.jsx
+++ frontend/src/pages/admin/AdminLogin.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { Link } from "react-router-dom"
+import { host } from "../../variables"
 
 export default function AdminLogin() {
 
@@ -11,7 +12,7 @@ export default function AdminLogin() {
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch(`${host}/api/auth/login`, {
       method: "POST",
       headers: {"Content-Type": "application/json"},
       body: JSON.stringify({username, password})
