0c06683c4e7528d992d896863739c2f2f94eb683
ElliotPa1m <elliot.palm@hotmail.com>
2026-09-19 10:34:27 +0200

    made the logo into a "home" button I think

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import logo from '../assets/logo.png'
 export default function Navbar() {
     return(
         <nav className="navbar">
-            <img src={logo} alt="logo"/>
+            <Link to="/productspage"><img src={logo} alt="logo"/></Link>
             <Link className="cart-link" to="/cart">Cart</Link>
         </nav>
     )
